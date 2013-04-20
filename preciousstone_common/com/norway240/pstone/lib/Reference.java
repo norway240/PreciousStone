db99e8f909c166f9c0fb0de0f425e6f25ab01c7f
norway240 <conman240@gmail.com>
2013-04-20 00:33:03 -0500

    First few base files

---
+++ preciousstone_common/com/norway240/pstone/lib/Reference.java
@@ -0,0 +1,8 @@
+package com.norway240.pstone.lib;
+
+public class Reference {
+    
+    public static final String MOD_ID = "PSTONE";
+    public static final String MOD_NAME = "Precious Stone";
+    public static final String VERSION = "0.0.1";
+}
